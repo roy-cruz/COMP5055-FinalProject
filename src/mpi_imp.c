f183ea951ef25d4a6cf192fb4297d2f9f026a633
roy <roy.cruz@upr.edu>
2023-04-23 12:12:29 -0400

    Mpi implementation works. Only problem is convoluted image has horizontal artifacts dividing the image in the number of processes involved

--- src/mpi_imp.c
+++ src/mpi_imp.c
@@ -2,49 +2,62 @@
 #include <fstream>
 #include <tiffio.h>
 #include <vector>
-#include <array>
-#include <cstdlib>
 #include <cmath>
+#include <string>
+#include <cstring>
 #include <mpi.h>
 
 void err_check(bool is_err, char* err_msg);
-void conv_tiff(TIFF* img, const float (&kernel)[9]);
-float sigmoid(float x) {
-    return (255.0/(1.0 + exp(-x)));
-};
+void conv_tiff(TIFF* tiff, const float (&edge_x)[9], const float (&edge_y)[9]);
+float sigmoid(float x);
 
+int numprocd;
 
 int main(int argc, char* argv[]) {
     bool is_err = false; 
 
     // Check that at least one argument is given.
-    if (argc < 2) {
-        is_err = true;
-    }
+    is_err = argc < 2;
     err_check(is_err, "ERROR: No imput files.");
 
-    // Initialize vector with filenames.
-    std::vector<std::string> fnames;
+    // Initialize vector TIFF pointers
+    std::vector<TIFF*> tiff_points;
     std::ifstream file_point;
     for (int i = 1; i < argc; i++) {
         file_point.open(argv[i]);
         if (file_point) {
-            fnames.push_back(std::string(argv[i]));
+            tiff_points.push_back(TIFFOpen((argv[i]), "r"));
+        } else {
+            std::cout << "WARNING: \'" << argv[i] << "\' could not be opened." << std::endl;
         }
         file_point.close();
-    }
+    };
 
-    const float kernel[9] = {
-        -1.0, -1.0, -1.0,
-        -1.0, 8.0, -1.0,
+    // Check that at least one tiff was opened.
+    is_err = (tiff_points.size() == 0);
+    err_check(is_err, "ERROR: No tiff file could be opened.");
+
+    // Prewitt operators (i.e. kernels) for edge detection (i.e. gradient magnitude calculation)
+    const float edge_x[9] = {
+        1.0, 0.0, -1.0,
+        1.0, 0.0, -1.0,
+        1.0, 0.0, -1.0
+    };
+
+    const float edge_y[9] = {
+        1.0, 1.0, 1.0,
+        0.0, 0.0, 0.0,
         -1.0, -1.0, -1.0
     };
 
-    for (int j = 0; j < fnames.size(); j++) {
-        TIFF* img = TIFFOpen(fnames[j].c_str(), "r");
-        conv_tiff(img, kernel);
-        TIFFClose(img);
-    }
+    numprocd = 0;
+
+    //Convolute all the tiffs! Also close each tiff after processing it.
+    for (int j = 0; j < tiff_points.size(); j++) {
+        conv_tiff(tiff_points[j], edge_x, edge_y);
+        TIFFClose(tiff_points[j]);
+        numprocd++;
+    };
     
     return 0;
 }
@@ -56,327 +69,153 @@ void err_check(bool is_err, char* err_msg){
     };    
 };
 
-void conv_tiff(TIFF* tiff, const float (&kernel)[9]) {
-    uint32_t columns, rows; // columns -> number of columns, rows -> number of rows
-    int rank, size;
+void conv_tiff(TIFF* tiff, const float (&kernel_x)[9], const float (&kernel_y)[9]) {
+    // Get number of rows and number of columns
+    uint32_t width, height; 
+    TIFFGetField(tiff, TIFFTAG_IMAGELENGTH, &height);
+    TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &width);
 
+    // // Make space in memory for input buffer and output buffer.
+    // uint32_t* inbuff = (uint32_t*) _TIFFmalloc(height * width * sizeof(uint32_t));
+    // uint32_t* outbuff = (uint32_t*) _TIFFmalloc(height * width * sizeof(uint32_t));
+
+    // // Read the image data from tiff and store it in inbuff
+    // TIFFReadRGBAImage(tiff, width, height, inbuff, 0);
+
+    int my_rank, comm_sz;
     MPI_Init(NULL, NULL);
-    MPI_Comm_size(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-    TIFFGetField(tiff, TIFFTAG_IMAGELENGTH, &rows);
-    TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &columns);
+    // Create a node-local communicator
+    MPI_Comm nodecomm; // Declare communicator object (group of processes)
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &nodecomm); 
+    // ^ Create communicator from MPI_COMM_WORLD
 
-    if (rank == 0) {
-        uint32_t* inbuff = (uint32_t*) _TIFFmalloc(rows * columns * sizeof(uint32_t));
-        uint32_t* outbuff = (uint32_t*) _TIFFmalloc(rows * columns * sizeof(uint32_t));
+    // Create a share memory window for inbuff
+    MPI_Win win_in; // Declare window object
+    uint32_t* baseptr_in;
+    MPI_Win_allocate_shared(height * width * sizeof(uint32_t), sizeof(uint32_t), MPI_INFO_NULL, nodecomm, &baseptr_in, &win_in);
+    // ^ Create shared memory window by allocating memory for each process on the same node
 
-        TIFFReadRGBAImage(tiff, columns, rows, inbuff);
+    // Create a shared memory window for outbuff
+    MPI_Win win_out;
+    uint32_t* baseptr_out;
+    MPI_Win_allocate_shared(height * width * sizeof(uint32_t), sizeof(uint32_t), MPI_INFO_NULL, nodecomm, &baseptr_out, &win_out);
+
+    // Get a pointer to the shared memory window for inbuff
+    int disp_unit_in;
+    MPI_Aint size_in; // MPI_Aint -> represetns address integer in MPI
+    uint32_t* shmptr_in;
+    MPI_Win_shared_query(win_in, 0, &size_in, &disp_unit_in, &shmptr_in);
+    // ^ Queries process-local address for remote memory segment created with MPI_Win_allocate_shared
+
+    // Get a pointer to the shared memory window for outbuff
+    int disp_unit_out;
+    MPI_Aint size_out;
+    uint32_t* shmptr_out;
+    MPI_Win_shared_query(win_out, 0, &size_out, &disp_unit_out, &shmptr_out);
+
+    if (my_rank == 0) {
+        TIFFReadRGBAImage(tiff, width, height, shmptr_in, 0);
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(nodecomm);
 
-    uint32_t chunck_size = rows / size;
-    uint32_t remainder = rows % size;
-    uint32_t my_start_row, my_num_rows;
-    if (rank < remainder) {
-        my_num_rows = chunck_size + 1;
-        my_start_row = rank * my_num_rows;
-    } else {
-        my_num_rows = chunck_size;
-        my_start_row = rank * my_num_rows + remainder;
+    int my_numrows = height/comm_sz; // Number of rows assigned to this process
+    int my_startrow = my_numrows * my_rank;
+    int rowsleft = height % comm_sz;
+    if ((my_rank == (comm_sz - 1)) & (rowsleft != 0)) {
+        my_numrows += rowsleft;
     }
+    int my_endrow = my_startrow + my_numrows - 1;
 
-    // Arrays for components
-    std::vector<uint8_t> r(my_num_rows * columns);
-    std::vector<uint8_t> g(my_num_rows * columns);
-    std::vector<uint8_t> b(my_num_rows * columns);
+    for (int i = my_startrow; i < my_endrow; i++) {
+        for (int j = 0; j < width; j++) {
+            // Gradient value initialization.
+            float grad_x = 0.0f;
+            float grad_y = 0.0f;
 
-    for (int i = 0; i < rows * columns; i++) {
-        r[i] = TIFFGetR(inbuff[i]);
-        g[i] = TIFFGetG(inbuff[i]);
-        b[i] = TIFFGetB(inbuff[i]);
-    }
+            // Loop over kernel elements
+            for (int k = -1; k <= 1; k++) {
+                for (int l = -1; l <= 1; l++){
+                    // Get pixel coodinates in the input image
+                    int x = j + l;
+                    int y = i + k;
 
-    float sum_r, sum_g, sum_b, sum_a;
-    int radius = 1, kernel_size = 3; // Radius of convolution kernel and size of kernel
-    int pixel_x, pixel_y, pixel_index;
-    float weight;
-    uint32_t conv_pixel;
-
-    // Convolution
-    for (int j = 0; j < rows; j++) {
-        for (int i = 0; i < columns; i++) {
-            sum_r = 0; sum_g = 0.0; sum_b = 0.0;
-            for (int y = -radius; y <= radius; y++) {
-                for (int x = -radius; x <= radius; x++) {
-                    pixel_y = j + y;
-                    pixel_x = i + x;
-                    if (pixel_x >= 0 && pixel_x < columns && pixel_y >= 0 && pixel_y < rows) { // Check that its not over the edge
-                        pixel_index = pixel_y * columns + pixel_x;
-                        weight = kernel[(y + radius) * kernel_size + (x + radius)];
-                        sum_r += r[pixel_index] * weight;
-                        sum_g += g[pixel_index] * weight;
-                        sum_b += b[pixel_index] * weight;
+                    // Clamp the coordinates to the image boundaries
+                    x = std::max(0, std::min(x, (int)width - 1));
+                    if ((my_rank == 0) || (my_rank == comm_sz - 1)) {
+                        y = std::max(0, std::min(y, (int)height - 1));
                     }
+
+                    // Get pixel value in the input image
+                    uint32_t pixel = shmptr_in[y * width + x];
+
+                    uint8_t r = TIFFGetR(pixel);
+                    uint8_t g = TIFFGetG(pixel);
+                    uint8_t b = TIFFGetB(pixel);
+
+                    // Convert pixel value to grayscale
+                    float gray = 0.2126f * r + 0.7152f * g + 0.0722f * b;
+
+                    // Multiply pixel value with corresponding kernel element and add to the gradient values
+                    grad_x += gray * kernel_x[(k + 1) * 3 + (l + 1)];
+                    grad_y += gray * kernel_y[(k + 1) * 3 + (l + 1)];
                 }
             }
 
-            int sum_rc = uint32_t(sigmoid(sum_r));
-            int sum_gc = uint32_t(sigmoid(sum_g));
-            int sum_bc = uint32_t(sigmoid(sum_b));
+            // Compute magnitude of gradient
+            float grad = sqrt(grad_x * grad_x + grad_y * grad_y);
 
-            // At this point, we have calculated one of the convoluted picture's pixel. We can proceed to store the calculated values.
-            conv_pixel = (sum_rc << 16) | (sum_gc << 8) | sum_bc;
+            //Restrict value to [0, 255] using sigmoid
+            grad = 255 * sigmoid(grad);
 
-            // Store result in outbuff
-            outbuff[j * columns + i] = conv_pixel;
+            //Convert magnitud to integer value
+            uint8_t edge = (uint8_t)grad;
 
+            // Set output pixel value to the edge value in all channels;
+            // outbuff[i * width + j] = TIFFRGBAImagePackRGBA(edge, edge, edge, 255); // Likely source of error
+            shmptr_out[i * width + j] = (edge << 24) | (edge << 16) | (edge << 8) | (255); 
         }
     }
 
-    TIFF* outimg = TIFFOpen("conv_img.tif", "w");
-    if (outimg) {
-        TIFFSetField(outimg, TIFFTAG_IMAGEWIDTH, columns);
-        TIFFSetField(outimg, TIFFTAG_IMAGELENGTH, rows);
-        TIFFSetField(outimg, TIFFTAG_SAMPLESPERPIXEL, 3);
-        TIFFSetField(outimg, TIFFTAG_BITSPERSAMPLE, 16);
-        TIFFSetField(outimg, TIFFTAG_PLANARCONFIG, 2);
-        TIFFSetField(outimg, TIFFTAG_PHOTOMETRIC, 2);
-        TIFFWriteEncodedStrip(outimg, 0, outbuff, columns * rows * sizeof(uint32_t));
-        TIFFClose(outimg);
+    MPI_Barrier(nodecomm);
+
+    if (my_rank == 0) {
+        std::string fname_str = "./conv_img_";
+        fname_str += std::to_string(numprocd);
+        fname_str += "_mpi.tiff";
+
+        char fname[100];
+        std::strcpy(fname, fname_str.c_str());
+
+        // Construct tiff from outbuff.
+        TIFF* outimg = TIFFOpen(fname, "w");
+        if (outimg) {
+            TIFFSetField(outimg, TIFFTAG_IMAGEWIDTH, width);
+            TIFFSetField(outimg, TIFFTAG_IMAGELENGTH, height);
+            TIFFSetField(outimg, TIFFTAG_BITSPERSAMPLE, 8);
+            TIFFSetField(outimg, TIFFTAG_SAMPLESPERPIXEL, 4);
+            TIFFSetField(outimg, TIFFTAG_PLANARCONFIG, PLANARCONFIG_CONTIG);
+            TIFFSetField(outimg, TIFFTAG_PHOTOMETRIC, PHOTOMETRIC_RGB);
+
+            for (int i = 0; i < height; i ++) {
+                // Write one scanline at a time
+                TIFFWriteScanline(outimg, &shmptr_out[(height - i - 1) * width], i);
+            }
+            TIFFClose(outimg);
+        }
     }
 
-    delete[] inbuff;
-    delete[] outbuff;
-
+    MPI_Barrier(nodecomm);
+    MPI_Win_free(&win_in);
+    MPI_Win_free(&win_out);
+    MPI_Finalize();
+    
     std::cout << "Yay!" << std::endl;
-
-
 }
 
-    // Initialize MPI
-
-    // Extract the image data from the tiff and store in an uint32_t array (inbuff)
-
-    // Share the address to the uint32_t array to all cores. Also share the address of the output array (outbuff)
-
-    // Calculate what section of the data each core is responsible for.
-
-    // Each core does a convolution on its section and stores the result in outbuff
-    
-    // Load each file and send them to be convoluted
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// #include <iostream>
-// #include <fstream>
-// #include <tiffio.h>
-// #include <vector>
-// #include <array>
-// #include <cstdlib>
-// #include <cmath>
-// #include <mpi.h>
-
-// void err_check(bool is_err, char* err_msg);
-// void start_process(std::vector<std::string> fnames);
-// void conv_tiff(TIFF* img, const float (&kernel)[9]);
-// float sigmoid(float x);
-
-
-// int main(int argc, char* argv[]) {
-//     bool is_err = false; 
-
-//     // Check that at least one argument is given.
-//     if (argc < 2) {
-//         is_err = true;
-//     }
-//     err_check(is_err, "ERROR: No imput files.");
-
-//     // Initialize vector with filenames.
-//     std::vector<std::string> fnames;
-//     std::ifstream file_point;
-//     for (int i = 1; i < argc; i++) {
-//         file_point.open(argv[i]);
-//         if (file_point) {
-//             fnames.push_back(std::string(argv[i]));
-//         }
-//         file_point.close();
-//     }
-
-//     start_process(fnames);
-    
-//     return 0;
-// }
-
-// void err_check(bool is_err, char* err_msg){
-//     if (is_err == true) {
-//         std::cerr << err_msg << std::endl;
-//         exit(1);
-//     };    
-// };
-
-// void start_process(std::vector<std::string> fnames){
-
-//     // Initialize kernel
-//     const float kernel[9] = {
-//         -1.0, -1.0, -1.0,
-//         -1.0, 8.0, -1.0,
-//         -1.0, -1.0, -1.0
-//     };
-
-//     // Load each file and send them to be convoluted
-//     for (int j = 0; j < fnames.size(); j++) {
-//         if (rank == 0) {
-//             TIFF* img = TIFFOpen(fnames[j].c_str(), "r");
-//         }
-//         conv_tiff(img, kernel);
-//     }
-// };
-
-// void conv_tiff(TIFF* tiff, const float (&kernel)[9]) {
-//     uint32_t columns, rows; // columns -> number of columns, rows -> number of rows
-//     int rank, size, bits_per_sample, samples_per_pixel;
-
-//     //Initialize MPI
-//     MPI_Init(NULL, NULL);
-//     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-//     TIFFGetField(tiff, TIFFTAG_IMAGELENGTH, &rows);
-//     TIFFGetField(tiff, TIFFTAG_IMAGEWIDTH, &columns);
-
-//     uint32_t chunck_size = rows / size;
-//     uint32_t remainder = rows % size;
-//     uint32_t my_start_row, my_num_rows;
-//     if (rank < remainder) {
-//         my_num_rows = chunck_size + 1;
-//         my_start_row = rank * my_num_rows;
-//     } else {
-//         my_num_rows = chunck_size;
-//         my_start_row = rank * my_num_rows + remainder;
-//     }
-
-//     // Each core allocates enough memory to use for itself
-//     uint32_t row_size = columns * sizeof(uint32_t);
-//     uint32_t buff_size = my_num_rows * row_size;
-//     uint32_t* inbuff = (uint32_t*) _TIFFmalloc(buff_size);
-//     uint32_t* outbuff = (uint32_t*) _TIFFmalloc(buff_size); 
-
-//     // Each core stores the part of the image that corresponds to it
-//     uint32_t current_row = my_start_row;
-//     for (uint32_t i = 0; i < my_num_rows; i++) {
-//         TIFFReadScanline(tiff, inbuff + i * columns, current_row);
-//         current_row++;
-//     }
-
-//     TIFFClose(tiff);
-
-
-
-
-
-
-//     if (rank == 0) {
-//         uint32_t* inbuff = (uint32_t*) _TIFFmalloc(rows * columns * sizeof(uint32_t));
-//         uint32_t* outbuff = (uint32_t*) _TIFFmalloc(rows * columns * sizeof(uint32_t));
-        
-//         // Arrays for components
-//         std::vector<uint8_t> r(rows * columns);
-//         std::vector<uint8_t> g(rows * columns);
-//         std::vector<uint8_t> b(rows * columns);
-
-        
-//     }
-
-
-    
-
-
-
-//     for (int i = 0; i < rows * columns; i++) {
-//         r[i] = TIFFGetR(inbuff[i]);
-//         g[i] = TIFFGetG(inbuff[i]);
-//         b[i] = TIFFGetB(inbuff[i]);
-//     }
-
-//     float sum_r, sum_g, sum_b, sum_a;
-//     int radius = 1, kernel_size = 3; // Radius of convolution kernel and size of kernel
-//     int pixel_x, pixel_y, pixel_index;
-//     float weight;
-//     uint32_t conv_pixel;
-
-//     // Convolution
-//     for (int j = 0; j < rows; j++) {
-//         for (int i = 0; i < columns; i++) {
-//             sum_r = 0; sum_g = 0.0; sum_b = 0.0;
-//             for (int y = -radius; y <= radius; y++) {
-//                 for (int x = -radius; x <= radius; x++) {
-//                     pixel_y = j + y;
-//                     pixel_x = i + x;
-//                     if (pixel_x >= 0 && pixel_x < columns && pixel_y >= 0 && pixel_y < rows) { // Check that its not over the edge
-//                         pixel_index = pixel_y * columns + pixel_x;
-//                         weight = kernel[(y + radius) * kernel_size + (x + radius)];
-//                         sum_r += r[pixel_index] * weight;
-//                         sum_g += g[pixel_index] * weight;
-//                         sum_b += b[pixel_index] * weight;
-//                     }
-//                 }
-//             }
-
-//             int sum_rc = uint32_t(sigmoid(sum_r));
-//             int sum_gc = uint32_t(sigmoid(sum_g));
-//             int sum_bc = uint32_t(sigmoid(sum_b));
-
-//             // At this point, we have calculated one of the convoluted picture's pixel. We can proceed to store the calculated values.
-//             conv_pixel = (sum_rc << 16) | (sum_gc << 8) | sum_bc;
-
-//             // Store result in outbuff
-//             outbuff[j * columns + i] = conv_pixel;
-
-//         }
-//     }
-
-//     TIFF* outimg = TIFFOpen("conv_img.tif", "w");
-//     if (outimg) {
-//         TIFFSetField(outimg, TIFFTAG_IMAGEWIDTH, columns);
-//         TIFFSetField(outimg, TIFFTAG_IMAGELENGTH, rows);
-//         TIFFSetField(outimg, TIFFTAG_SAMPLESPERPIXEL, 3);
-//         TIFFSetField(outimg, TIFFTAG_BITSPERSAMPLE, 16);
-//         // TIFFSetField(outimg, TIFFTAG_ORIENTATION, ORIENTATION_TOPLEFT);
-//         TIFFSetField(outimg, TIFFTAG_PLANARCONFIG, 2);
-//         TIFFSetField(outimg, TIFFTAG_PHOTOMETRIC, 2);
-//         TIFFWriteEncodedStrip(outimg, 0, outbuff, columns * rows * sizeof(uint32_t));
-//         TIFFClose(outimg);
-//     }
-
-//     delete[] inbuff;
-//     delete[] outbuff;
-
-//     std::cout << "Yay!" << std::endl;
-// }
-
-// float sigmoid(float x) {
-//     return (255.0/(1.0 + exp(-x)));
-// }
+float sigmoid(float x) {
+    return (1/(1.0 + exp(-x)));
+}
